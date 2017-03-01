5bac2bb2747c0089259193341302188ad675b1cc
verenaseverina <verenaseverina@gmail.com>
2017-03-01 09:07:36 +0700

    Ditambahin Cage.h

---
+++ Cage.h
@@ -0,0 +1,39 @@
+// File: Cage.h
+
+#ifndef CAGE_H
+#define CAGE_H
+
+class Cage {
+public:
+	/**
+	 * @brief Constructor.
+	 * Menciptakan Cage kosong.
+	 */
+	Cage();
+
+	/**
+	 * @brief CopyConstructor.
+	 * Membuat salinan dari Cage lain ke objek
+	 * @param c adalah Cage yang hendak disalin
+	 */
+	Cage(const Cage& c);
+
+	/**
+	 * @brief Destructor.
+	 */
+	~Cage();
+
+	/**
+	 * @brief Overloading Operator "="
+	 * Menambahkan fungsi pada operator "=" agar penyalinan tidak terjadi secara bit size
+	 * @param c adalah Cage yang hendak disalin
+	 */
+	Cage& operator=(const Cage& c);
+
+
+private:
+	Habitat* p;
+	//char Habitat;
+	int size;
+};
+#endif
--- Cell.cpp
+++ Cell.cpp
@@ -1,5 +1,4 @@
-#include "Cell.h"
-#include <cstddef>
+ #include "Cell.h"
 
 Cell::Cell()
 {
