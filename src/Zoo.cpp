2059826b710d482969a358367c78708b040d0e30
adrianhp97 <adrianhp100797@gmail.com>
2017-03-13 22:15:44 +0700

    header

--- src/Zoo.cpp
+++ src/Zoo.cpp
@@ -1,323 +1,337 @@
-//File: Zoo.cpp
+//File: zoo.cpp
 #include <iostream>
-#include "Zoo.h"
+#include "zoo.h"
 using namespace std;
-	Zoo::Zoo()
-	{
-		State s;
-		char** smap=s.GetMap();
-		height=s.GetHeight();
-		width=s.GetWidth();
-		//buat matriks cell kosong
-		map = new Cell**[height];
-		for(int i=0;i<height;i++)
-			map[i] = new Cell*[width];
-		//spesifikasi matriks cell
-		for(int i=0;i<height;i++)
-		{
-			for(int j=0;j<width;j++)
-			{
-				if(IsHabitat(smap[i][j]))
-					map[i][j] = new Habitat(smap[i][j],i,j);
-				else if(IsFacility(smap[i][j]))
-					map[i][j] = new Facility(smap[i][j],i,j);
-			}
-		}
-		GetCage(map[1][3]);
-		//GetAllCage(s);
-	}
+Zoo::Zoo()
+{
+  State s;
+  char** smap=s.GetMap();
+  height=s.GetHeight();
+  width=s.GetWidth();
+  //buat matriks cell kosong
+  map = new Cell**[height];
+  for(int i=0;i<height;i++)
+    map[i] = new Cell*[width];
+  //spesifikasi matriks cell
+  for(int i=0;i<height;i++)
+  {
+    for(int j=0;j<width;j++)
+    {
+      if(IsHabitat(smap[i][j]))
+        map[i][j] = new Habitat(smap[i][j],i,j);
+      else if(IsFacility(smap[i][j]))
+        map[i][j] = new Facility(smap[i][j],i,j);
+    }
+  }
+  GetCage(map[1][3]);
+  //GetAllCage(s);
+}
 
-	Zoo::~Zoo(){
-		for(int i=0;i<height;i++){
-			for(int j=0;j<width;j++){
-				delete map[i][j];			
-			}
-			delete map[i];		
-		}
-		delete map;
-	}
+Zoo::~Zoo(){
+  for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+       delete map[i][j];			
+    }
+    delete map[i];		
+  }
+  delete map;
+}
 
-	void Zoo::GetAllCage(State& s)
-	{
-		vector<vector<Habitat>> cagebuffer;
-		char** smap=s.GetMap();
-		int n=0;
-		for(int i=0;i<height;i++)
-		{
-			for(int j=0;j<width;j++)
-			{
-				if(IsHabitat(smap[i][j]))
-				{
-					bool recorded=false;
-					int x=0;
-					while((!recorded) && (x<(signed)cagebuffer.size()))
-					{
-						recorded=InCage(cagebuffer[x],map[i][j]);
-						x++;
-					}
-					if(!recorded)
-					{
-						//cout << cagebuffer.size();
-						cagebuffer.resize(n);
-						//cagebuffer[n]=GetCage(map[i][j]);
-						n++;
-					}
-				}
-			}
-		}
-		//cages.resize(10);
-		for(int i=0;i<(signed)cagebuffer.size();i++)
-		{
-			Cage cgbuf((int)cagebuffer.size(),cagebuffer[i]);
-			cages.push_back(cgbuf);
-		}
-	}
-	vector<Habitat> Zoo::GetCage(Cell* hab)
-	{
-		vector<Habitat> cage(1);
-		Habitat h;
-		cage[0]=hab;
-		int i=0;
-		int j=0;
-		cout << cage[0].GetCellContent()<< endl;
-		cout << hab->GetCellContent() << endl;
-		while(i<(signed)cage.size())
-		{
-			h=cage[i];
-			if(map[h.GetCellRow()-1][h.GetCellCol()]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i-1][j])&&(hab->GetCellRow()-1>=0))
-			{
-				//cage.resize(cage.size()+1);
-				Habitat b(map[h.GetCellRow()-1][h.GetCellCol()]->GetCellContent(),h.GetCellRow()-1,h.GetCellCol());				
-				cage.push_back(b);
-			}
-			if(map[h.GetCellRow()][h.GetCellCol()+1]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i][j+1]))
-			{
-				cage.resize(cage.size()+1);
-				j++;
-				cage[j]=h;
-			}
-			if(map[h.GetCellRow()+1][h.GetCellCol()]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i+1][j]))
-			{
-				cage.resize(cage.size()+1);
-				j++;
-				cage[j]=h;
-			}
-			if(map[h.GetCellRow()][h.GetCellCol()-1]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i][j-1]))
-			{
-				cage.resize(cage.size()+1);
-				j++;
-				cage[j]=h;
-			}
-			i++;
-		}
-		return cage;
-		
-	}
+void Zoo::GetAllCage(State& s)
+{
+  vector<vector<Habitat>> cagebuffer;
+  char** smap=s.GetMap();
+  int n=0;
+  for(int i=0;i<height;i++)
+  {
+    for(int j=0;j<width;j++)
+    {
+      if(IsHabitat(smap[i][j]))
+      {
+        bool recorded=false;
+        int x=0;
+        while((!recorded) && (x<(signed)cagebuffer.size()))
+        {
+          recorded=InCage(cagebuffer[x],map[i][j]);
+          x++;
+          
+        if(!recorded)
+        {
+          //cout << cagebuffer.size();
+          cagebuffer.resize(n);
+          //cagebuffer[n]=GetCage(map[i][j]);
+          n++;
+        }
+      }
+    }
+  }
+  //cages.resize(10);
+  for(int i=0;i<(signed)cagebuffer.size();i++)
+  {
+    Cage cgbuf((int)cagebuffer.size(),cagebuffer[i]);
+    cages.push_back(cgbuf);
+  }
+}
+vector<Habitat> Zoo::GetCage(Cell* hab)
+{
+  vector<Habitat> cage(1);
+  Habitat h;
+  cage[0]=hab;
+  int i=0;
+  int j=0;
+  cout << cage[0].GetCellContent()<< endl;
+  cout << hab->GetCellContent() << endl;
+  while(i<(signed)cage.size())
+  {
+    h=cage[i];
+    if(map[h.GetCellRow()-1][h.GetCellCol()]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i-1][j])&&(hab->GetCellRow()-1>=0))
+    {
+      //cage.resize(cage.size()+1);
+      Habitat b(map[h.GetCellRow()-1][h.GetCellCol()]->GetCellContent(),h.GetCellRow()-1,h.GetCellCol());				
+      cage.push_back(b);
+    }
+    if(map[h.GetCellRow()][h.GetCellCol()+1]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i][j+1]))
+    {
+      cage.resize(cage.size()+1);
+      j++;
+      cage[j]=h;
+    }
+    if(map[h.GetCellRow()+1][h.GetCellCol()]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i+1][j]))
+    {
+      cage.resize(cage.size()+1);
+      j++;
+      cage[j]=h;
+    }
+    if(map[h.GetCellRow()][h.GetCellCol()-1]->GetCellContent()==hab->GetCellContent()&&!InCage(cage,map[i][j-1]))
+    {
+      cage.resize(cage.size()+1);
+      j++;
+      cage[j]=h;
+    }
+    i++;
+  }
+  return cage;
 	
-	bool Zoo::InCage(vector<Habitat>& v, Cell* c)
-	{
-		if(IsHabitat(c->GetCellContent()))
-		{
-			bool found=false;
-			int i=0;
-			while(!found && i<(signed)v.size())
-			{
-				if(c->GetCellRow()==v[i].GetCellRow() && c->GetCellCol()==v[i].GetCellCol())
-					found=true;
-				i++;		
-			}
-			return(found);
-		}
-		else 
-		{
-			return(false);
-		}
-	}
-	
-	Cell*** Zoo::GetMap()
-	{
-		return map;
-	}
+}
 
-	vector<Cage> Zoo::GetCages()
-	{
-		return cages;
-	}
+bool Zoo::InCage(vector<Habitat>& v, Cell* c)
+{
+  if(IsHabitat(c->GetCellContent()))
+  {
+    bool found=false;
+    int i=0;
+    while(!found && i<(signed)v.size())
+    {
+      if(c->GetCellRow()==v[i].GetCellRow() && c->GetCellCol()==v[i].GetCellCol())
+        found=true;
+      i++;		
+    }
+    return(found);
+  }
+  else 
+  {
+    return(false);
+  }
+}
 
-	int Zoo::GetHeight()
-	{
-		return(height);
-	}
-	
-	int Zoo::GetWidth()
-	{
-		return(width);
-	}
-	
-	bool Zoo::IsHabitat(char c)
-	{
-		return(c=='^'||c=='~'||c=='`');
-	}
-	bool Zoo::IsFacility(char c)
-	{
-		return(c=='#'||c=='_'||c=='R');
-	}
-	void Zoo::MasukkanAnimal()
-	{
-		int size = cages.size();
-		cout<<"Nama-Nama Binatang dan kodenya" << endl;
-		cout<<"- Tiger (H)"<<endl;
-		cout<<"- Panda (B)"<<endl;
-		cout<<"- Anoa (A)"<<endl;
-		cout<<"- Rhino (R)"<<endl;
-		cout<<"- Kangaroo (D)"<<endl;
-		cout<<"- Dolphin (L)"<<endl;
-		cout<<"- Whale (W)"<<endl;
-		cout<<"- Shark (S)"<<endl;
-		cout<<"- Kelelawar (K)"<<endl;
-		cout<<"- Elang Botak (E)"<<endl;
-		cout<<"- Toucan (T)"<<endl;
-		cout<<"- Penguin (P)"<<endl;
-		cout<<"- Crocodile (C)"<<endl;
-		cout<<"- Hippopotamus (N)"<<endl;
-		cout<<endl;
+Cell*** Zoo::GetMap()
+{
+  return map;
+}
 
-		char inputuser;
-		cout<<"Masukkan kode binatang yang ingin dimasukkan kedalam kandang : "<<endl;
-		cin>>inputuser;
+vector<Cage> Zoo::GetCages()
+{
+  return cages;
+}
 
-		bool* arr= new bool [size];
-		arr={false};
-		switch(inputuser)
-		{
-			case 'H' :
-				{
-					Tiger H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'B' :
-				{
-					Panda H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'A' :
-				{
-					Anoa H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'R' :
-				{
-					Rhino H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'D' :
-				{
-					Kangaroo H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'L' :
-				{
-					Dolphin H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'W' :
-				{
-					Whale H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'S' :
-				{
-					Shark H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'K' :
-				{
-					Kelelawar H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'E' :
-				{
-					ElangB H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'T' :
-				{
-					Toucan H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'P' :
-				{
-					Penguin H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'C' :
-				{
-					Crocodile H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-			case 'N' :
-				{
-					Hippopotamus H(-1,-1,-1);
-					CekCage(arr,&H);
-				}
-		}
+int Zoo::GetHeight()
+{
+  return(height);
+}
 
-		cout<<"Kandang yang tersedia :"<<endl;
-		for (int i=0;i<size;i++)
-		{
-			if (arr[i])
-			{
-				cout<<i<<endl;
-			}
-		}
-		cout<<endl;
-		int x;
-		cout<<"Masukkan kode binatang yang ingin dimasukkan kedalam kandang : "<<endl;
-		cout<<"(Untuk kandang bebas, masukkan -1)"<<endl;
-		cout<<"(Untuk membatalkan, masukkan -2)"<<endl;
-		do
-		{
-			cin>>x;
-			if((arr[x]==false)&&(x>=size)&&(x!=-1)&&(x!=-2))
-			{
-				cout<<"Input salah, masukkan kembali input :"<<endl;
-			}
-		}while((arr[x]==false)&&(x>=size)&&(x!=-1)&&(x!=-2));
+int Zoo::GetWidth()
+{
+  return(width);
+}
 
-		if (x==-1)
-		{
-			srand(time(NULL));
-			do
-			{
-				x = rand() % size;
-			} while (arr[x]==false);
-		}
-		if (x!=-2)
-		{
-			cages[x].addAnimal(inputuser,x);
-		}
-		delete [] arr;
-	}
+bool Zoo::IsHabitat(char c)
+{
+  return(c=='^'||c=='~'||c=='`');
+}
+bool Zoo::IsFacility(char c)
+{
+  return(c=='#'||c=='_'||c=='R');
+}
+void Zoo::MasukkanAnimal()
+{
+  int size = cages.size();
+  cout<<"Nama-Nama Binatang dan kodenya" << endl;
+   cout<<"- Tiger (H)"<<endl;
+  cout<<"- Panda (B)"<<endl;
+  cout<<"- Anoa (A)"<<endl;
+  cout<<"- Rhino (R)"<<endl;
+  cout<<"- Kangaroo (D)"<<endl;
+  cout<<"- Dolphin (L)"<<endl;
+  cout<<"- Whale (W)"<<endl;
+  cout<<"- Shark (S)"<<endl;
+  cout<<"- Kelelawar (K)"<<endl;
+  cout<<"- Elang Botak (E)"<<endl;
+  cout<<"- Toucan (T)"<<endl;
+  cout<<"- Penguin (P)"<<endl;
+  cout<<"- Crocodile (C)"<<endl;
+  cout<<"- Hippopotamus (N)"<<endl;
+  cout<<endl;
 
-	void Zoo::CekCage(bool* arr,Animal* H)
-	{
-		for (int j=0;j<(*H).GetSize();j++)
-		{				
-			int i=0;
-			while(i<(signed)cages.size())
-			{
-				if ((arr[i]==false)&&((*H).GetHab()[j]==cages[i].GetAnimal()[0][0].GetHab()[j]))
-				{					
-					if(cages[i].isEmpty())
-					{
-						arr[i]=true;
-					}
-					else if((!cages[i].isFull())&&((*H).GetTame()==cages[i].GetAnimal()[0][0].GetTame()))
-					{
-						arr[i]=true;
-					}
-				}
-				i++;
-			}
-		}
-	}
+  char inputuser;
+  cout<<"Masukkan kode binatang yang ingin dimasukkan kedalam kandang : "<<endl;
+  cin>>inputuser;
+
+  bool* arr= new bool [size];
+  arr={false};
+  switch(inputuser)
+  {
+    case 'H' :
+      {
+	      Tiger H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'B' :
+      {
+	      Panda H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'A' :
+      {
+	      Anoa H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'R' :
+      {
+	      Rhino H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'D' :
+      {
+	      Kangaroo H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'L' :
+      {
+	      Dolphin H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'W' :
+      {
+	      Whale H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'S' :
+      {
+	      Shark H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'K' :
+      {
+	      Kelelawar H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'E' :
+      {
+	      ElangB H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'T' :
+      {
+	      Toucan H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'P' :
+      {
+	      Penguin H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'C' :
+      {
+	      Crocodile H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+    case 'N' :
+      {
+	      Hippopotamus H(-1,-1,-1);
+	      CekCage(arr,&H);
+	      break;
+      }
+  }
+
+  cout<<"Kandang yang tersedia :"<<endl;
+  for (int i=0;i<size;i++)
+  {
+    if (arr[i])
+    {
+      cout<<i<<endl;
+    }
+  }
+  cout<<endl;
+  int x;
+  cout<<"Masukkan kode binatang yang ingin dimasukkan kedalam kandang : "<<endl;
+  cout<<"(Untuk kandang bebas, masukkan -1)"<<endl;
+  cout<<"(Untuk membatalkan, masukkan -2)"<<endl;
+  do
+  {
+    cin>>x;
+    if((arr[x]==false)&&(x>=size)&&(x!=-1)&&(x!=-2))
+    {
+      cout<<"Input salah, masukkan kembali input :"<<endl;
+    }
+  }while((arr[x]==false)&&(x>=size)&&(x!=-1)&&(x!=-2));
+
+  if (x==-1)
+  {
+    srand(time(NULL));
+    do
+    {
+      x = rand() % size;
+    } while (arr[x]==false);
+  }
+  if (x!=-2)
+  {
+    cages[x].addAnimal(inputuser,x);
+  }
+  delete [] arr;
+}
+
+void Zoo::CekCage(bool* arr,Animal* H)
+{
+  for (int j=0;j<(*H).GetSize();j++)
+  {				
+    int i=0;
+    while(i<(signed)cages.size())
+    {
+      if ((arr[i]==false)&&((*H).GetHab()[j]==cages[i].GetAnimal()[0][0].GetHab()[j]))
+      {					
+        if(cages[i].isEmpty())
+        {
+          arr[i]=true;
+        }
+        else if((!cages[i].isFull())&&((*H).GetTame()==cages[i].GetAnimal()[0][0].GetTame()))
+        {
+          arr[i]=true;
+        }
+      }
+      i++;
+    }
+  }
+}
--- src/Zoo.h
+++ src/Zoo.h
@@ -1,105 +1,105 @@
-//File: Zoo.h
+//File: zoo.h
 
 #ifndef ZOO_H
 #define ZOO_H
 
 #include <vector>
-#include "State.h"
-#include "Cage.h"
-#include "Cell.h"
-#include "Habitat.h"
-#include "Facility.h"
+#include "state.h"
+#include "cage.h"
+#include "cell.h"
+#include "habitat.h"
+#include "facility.h"
 class Zoo
 {
 public:
-	/**
-	 * @brief Constructor.
-	 * Menginisialisasi map dan cage.
-	 */
-	Zoo();
-	/**
-     * @brief Destructor
-     * Melakukan dealokasi pada cage dan map
-     */ 
-	~Zoo();
-	/**
-	 * @brief Method GetAllCage
-     * Mengisi semua cage dari input state
-	 * @param s adalah kelas state yang berisi map zoo 
-	 */
-	void GetAllCage(State& s);
-	/**
-	 * @brief Method GetCage
-	 * Mengisi sebuah cage dengan pendekatan BFS 
-	 * @param h adalah koordinat awal habitat
-	 */
-	vector<Habitat> GetCage(Cell* h);
-	/**
-	 * @brief Method Incage
-	 * Mengeluarkan output apakah sebuah cage telah dikunjungi
-	 * @param v adalah kumpulan habitat yang telah dikunjungi
-	 * @param h adalah habitat yang ingin dilakukan pengecekan
-	 */
-	bool InCage(vector<Habitat>& v, Cell* c);
-	/**
-	 * @brief Getter Map
-	 * Mengembalikan matriks map dari Zoo
-	 * @return matriks map dari Zoo
-	 */
-	Cell*** GetMap();
-	/**
-	 * @brief Getter Cages
-	 * Mengembalikan atribut Cages dari Zoo
-	 * @return vektor Cages dari Zoo
-	 */
-	vector<Cage> GetCages();
-	/**
-	 * @brief Method GetHeight
-	 * Mengembalikan tinggi dari zoo
-	 */
-	int GetHeight();
-	/**
-	 * @brief Method GetWidth
-	 * Mengembalikan lebar dari zoo
-	 */
-	int GetWidth();
-	/**
-	 *  @brief Method IsHabitat
-	 *  Membandingkan apakah suatu karakter c termasuk habitat
-	 *  @param c adalah karakter pada map yang akan diperiksa
-	 *  @return boolean apakah karakter termasuk habitat
-	 */
-	bool IsHabitat(char c);
-	/**
-	 *  @brief Method IsFacility
-	 *  Membandingkan apakah suatu karakter c termasuk facility
-	 *  @param c adalah karakter pada map yang akan diperiksa
-	 *  @return boolean apakah karakter termasuk facility
-	 */
-	bool IsFacility(char c);
-	//Elemen constructor
-	//Pindahin semua State ke map
-	//void MasukkanAnimal();
-	//void CekCage(int* arr,Animal H);
-	/**
-	 *  @brief Method MasukkanAnimal
-	 *  
-	 *  @details More details
-	 */
-	void MasukkanAnimal();
-	/**
-	 *  @brief Method CekCage
-	 *  
-	 *  @param Animal 
-	 *  @param int* 
-	 *  @details More details
-	 */
-	void CekCage(bool*, Animal*);
+  /**
+   * @brief Constructor.
+   * Menginisialisasi map dan cage.
+   */
+  Zoo();
+  /**
+      * @brief Destructor
+      * Melakukan dealokasi pada cage dan map
+      */ 
+  ~Zoo();
+  /**
+   * @brief Method GetAllCage
+      * Mengisi semua cage dari input state
+   * @param s adalah kelas state yang berisi map zoo 
+   */
+  void GetAllCage(State& s);
+  /**
+   * @brief Method GetCage
+   * Mengisi sebuah cage dengan pendekatan BFS 
+   * @param h adalah koordinat awal habitat
+   */
+  vector<Habitat> GetCage(Cell* h);
+  /**
+   * @brief Method Incage
+   * Mengeluarkan output apakah sebuah cage telah dikunjungi
+   * @param v adalah kumpulan habitat yang telah dikunjungi
+   * @param h adalah habitat yang ingin dilakukan pengecekan
+   */
+  bool InCage(vector<Habitat>& v, Cell* c);
+  /**
+   * @brief Getter Map
+   * Mengembalikan matriks map dari Zoo
+   * @return matriks map dari Zoo
+   */
+  Cell*** GetMap();
+  /**
+   * @brief Getter Cages
+   * Mengembalikan atribut Cages dari Zoo
+   * @return vektor Cages dari Zoo
+   */
+  vector<Cage> GetCages();
+  /**
+   * @brief Method GetHeight
+   * Mengembalikan tinggi dari zoo
+   */
+  int GetHeight();
+  /**
+   * @brief Method GetWidth
+   * Mengembalikan lebar dari zoo
+   */
+  int GetWidth();
+  /**
+   *  @brief Method IsHabitat
+   *  Membandingkan apakah suatu karakter c termasuk habitat
+   *  @param c adalah karakter pada map yang akan diperiksa
+   *  @return boolean apakah karakter termasuk habitat
+   */
+  bool IsHabitat(char c);
+  /**
+   *  @brief Method IsFacility
+   *  Membandingkan apakah suatu karakter c termasuk facility
+   *  @param c adalah karakter pada map yang akan diperiksa
+   *  @return boolean apakah karakter termasuk facility
+   */
+  bool IsFacility(char c);
+  //Elemen constructor
+  //Pindahin semua State ke map
+  //void MasukkanAnimal();
+  //void CekCage(int* arr,Animal H);
+  /**
+   *  @brief Method MasukkanAnimal
+   *  
+   *  @details More details
+   */
+  void MasukkanAnimal();
+  /**
+   *  @brief Method CekCage
+   *  
+   *  @param Animal 
+   *  @param int* 
+   *  @details More details
+   */
+  void CekCage(bool*, Animal*);
 private:
-	vector<Cage> cages;
-	Cell*** map;
-	int height;
-	int width;
+  vector<Cage> cages;
+  Cell*** map;
+  int height;
+  int width;
 };
 
 #endif
