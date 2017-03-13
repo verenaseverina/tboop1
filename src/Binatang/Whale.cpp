70da0d5cd88b739ead933a5c3df2ab675af7d4ac
adrianhp97 <adrianhp100797@gmail.com>
2017-03-13 08:10:29 +0700

    whale doang

--- src/Binatang/Whale.cpp
+++ src/Binatang/Whale.cpp
@@ -15,3 +15,14 @@ string Whale::interact()
 {
 	return this->interaction;
 }
+
+Whale& Whale::operator=(const Whale& a)
+{
+	WaterAnimal::operator=(a);
+    interaction = a.interaction;
+    myWeight = a.myWeight;
+    howMuchIEat = a.howMuchIEat;
+    whatIEat = a.whatIEat;
+    isTame = a.isTame;
+    return *this;
+}
