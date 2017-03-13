95dc52e958b64ae597e6843766d1ff64afd270d2
adrianhp97 <adrianhp100797@gmail.com>
2017-03-13 22:36:24 +0700

    header

--- v2/src/animal.cpp
+++ v2/src/animal.cpp
@@ -1,303 +1,311 @@
 #include "animal.h"
 
 Animal::Animal(int _x,int _y, int id,char con){
-    x = _x;
-    y = _y;
-    cage_id = id;
-    content=con;
+  x = _x;
+  y = _y;
+  cage_id = id;
+  content=con;
 
-    switch(animal)
-    {
-        case 'H' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='x';
+  switch(animal)
+  {
+    case 'H' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='x';
 
-                interaction = "Roooarrrr";
-                srand(time(NULL));
-                my_weight = 90 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(310-90)));
-                how_much_i_eat = 0;
-                what_i_eat = 'c';
-                is_tame = false;
-                break;
-            }
-        case 'B' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='o';
+        interaction = "Roooarrrr";
+        srand(time(NULL));
+        my_weight = 90 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(310-90)));
+        how_much_i_eat = 0;
+        what_i_eat = 'c';
+        is_tame = false;
+        break;
+      }
+    case 'B' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='o';
                 
-                interaction = "Mmmm";
-                srand(time(NULL));
-                my_weight = 70 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100-70)));
-                how_much_i_eat = 38*my_weight/100;
-                what_i_eat = 'h';
-                is_tame = true;
-                break;
-            }
-        case 'A' :
-            {              
-                size=1;
-                hab=new char[size];
-                hab[0]='o';
+        interaction = "Mmmm";
+        srand(time(NULL));
+        my_weight = 70 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100-70)));
+        how_much_i_eat = 38*my_weight/100;
+        what_i_eat = 'h';
+        is_tame = true;
+        break;
+      }
+    case 'A' :
+      {              
+        size=1;
+        hab=new char[size];
+        hab[0]='o';
 
-                interaction = "Grrrr";
-                srand(time(NULL));
-                my_weight = 150 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(300-150)));
-                how_much_i_eat = 5*my_weight/100;
-                what_i_eat = 'h';
-                is_tame = true;
-                break;
-            }
-        case 'R' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='x';
+        interaction = "Grrrr";
+        srand(time(NULL));
+        my_weight = 150 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(300-150)));
+        how_much_i_eat = 5*my_weight/100;
+        what_i_eat = 'h';
+        is_tame = true;
+        break;
+      }
+    case 'R' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='x';
                
-                interaction = "Woooo";
-                srand(time(NULL));
-                my_weight = 900 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(2300-900)));
-                how_much_i_eat = 7.23*my_weight/100;
-                what_i_eat = 'h';
-                is_tame = false;
-                break;
-            }
-        case 'D' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='x';
+        interaction = "Woooo";
+        srand(time(NULL));
+        my_weight = 900 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(2300-900)));
+        how_much_i_eat = 7.23*my_weight/100;
+        what_i_eat = 'h';
+        is_tame = false;
+        break;
+      }
+    case 'D' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='x';
 
-                interaction = "Jummpp";
-                srand(time(NULL));
-                my_weight = 18 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100-18)));
-                how_much_i_eat = 0.35 * my_weight/100;
-                what_i_eat = 'h';
-                is_tame = true;
-                break;
-            }
-        case 'L' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='#';
+        interaction = "Jummpp";
+        srand(time(NULL));
+        my_weight = 18 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(100-18)));
+        how_much_i_eat = 0.35 * my_weight/100;
+        what_i_eat = 'h';
+        is_tame = true;
+        break;
+      }
+    case 'L' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='#';
 
-                interaction = "Cuitttt";
-                srand(time(NULL));
-                my_weight = 110 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200-110)));
-                how_much_i_eat = (0.04 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.09-0.04)))) * my_weight;
-                what_i_eat = 'c';
-                is_tame = true;
-                break;
-            }
-        case 'W' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='#';
+        interaction = "Cuitttt";
+        srand(time(NULL));
+        my_weight = 110 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200-110)));
+        how_much_i_eat = (0.04 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.09-0.04)))) * my_weight;
+        what_i_eat = 'c';
+        is_tame = true;
+        break;
+      }
+    case 'W' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='#';
 
-                interaction = "Wet";
-                srand(time(NULL));
-                my_weight = 14000 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(16000-14000)));
-                how_much_i_eat = 0.0667 * my_weight;
-                what_i_eat = 'c';
-                is_tame = true;
-                break;
-            }
-        case 'S' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='#';
+        interaction = "Wet";
+        srand(time(NULL));
+        my_weight = 14000 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(16000-14000)));
+        how_much_i_eat = 0.0667 * my_weight;
+        what_i_eat = 'c';
+        is_tame = true;
+        break;
+      }
+    case 'S' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='#';
 
-                interaction = "Wett";
-                srand(time(NULL));
-                my_weight = 21500 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(30000-21500)));
-                how_much_i_eat = 0.0667 * my_weight;
-                what_i_eat = 'c';
-                is_tame = false;
-                break;
-            }
-        case 'K' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='o';
+        interaction = "Wett";
+        srand(time(NULL));
+        my_weight = 21500 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(30000-21500)));
+        how_much_i_eat = 0.0667 * my_weight;
+        what_i_eat = 'c';
+        is_tame = false;
+        break;
+      }
+    case 'K' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='o';
 
-                interaction = "Kiiiaaa";
-                srand(time(NULL));
-                my_weight = 1.2 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1.4-1.2)));
-                how_much_i_eat = my_weight;
-                what_i_eat = 'o';
-                is_tame = true;
-                break;
-            }
-        case 'E' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='o';
+        interaction = "Kiiiaaa";
+        srand(time(NULL));
+        my_weight = 1.2 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(1.4-1.2)));
+        how_much_i_eat = my_weight;
+        what_i_eat = 'o';
+        is_tame = true;
+        break;
+      }
+    case 'E' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='o';
 
-                interaction = "Kiiiaaa";
-                srand(time(NULL));
-                my_weight = 4 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(6-4)));
-                how_much_i_eat = 0.4535*my_weight/4;
-                what_i_eat = 'c';
-                break;
-            }
-        case 'T' :
-            {
-                size=1;
-                hab=new char[size];
-                hab[0]='o';
+        interaction = "Kiiiaaa";
+        srand(time(NULL));
+        my_weight = 4 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(6-4)));
+        how_much_i_eat = 0.4535*my_weight/4;
+        what_i_eat = 'c';
+        break;
+      }
+    case 'T' :
+      {
+        size=1;
+        hab=new char[size];
+        hab[0]='o';
 
-                interaction = "Kwwaakk";
-                srand(time(NULL));
-                my_weight = 0.592 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.760-0.592)));
-                how_much_i_eat = 0.5*my_weight;
-                what_i_eat = 'o';
-                is_tame = true;
-                break;
-            }
-        case 'P' :
-            {
-                size=2;
-                hab=new char[size];
-                hab[0]='#';
-                hab[1]='x';
+        interaction = "Kwwaakk";
+        srand(time(NULL));
+        my_weight = 0.592 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(0.760-0.592)));
+        how_much_i_eat = 0.5*my_weight;
+        what_i_eat = 'o';
+        is_tame = true;
+        break;
+      }
+    case 'P' :
+      {
+        size=2;
+        hab=new char[size];
+        hab[0]='#';
+        hab[1]='x';
 
-                interaction = "Sqqqaaa";
-                srand(time(NULL));
-                my_weight = 27 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(41-27)));
-                how_much_i_eat = 0.9*my_weight/27;
-                what_i_eat = 'c';
-                is_tame = true;
-                break;
-            }
-        case 'C' :
-            {
-                size=2;
-                hab=new char[size];
-                hab[0]='x';
-                hab[1]='#';
+        interaction = "Sqqqaaa";
+        srand(time(NULL));
+        my_weight = 27 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(41-27)));
+        how_much_i_eat = 0.9*my_weight/27;
+        what_i_eat = 'c';
+        is_tame = true;
+        break;
+      }
+    case 'C' :
+      {
+        size=2;
+        hab=new char[size];
+        hab[0]='x';
+        hab[1]='#';
 
-                interaction = "Rrrrr";
-                srand(time(NULL));
-                my_weight = 400  + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(700-400)));
-                how_much_i_eat = 2*my_weight/700;
-                what_i_eat = 'c';
-                is_tame = false;
-                break;
-            }
-        case 'N' :
-            {
-                size=2;
-                hab=new char[size];
-                hab[0]='x';
-                hab[1]='#';
+        interaction = "Rrrrr";
+        srand(time(NULL));
+        my_weight = 400  + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(700-400)));
+        how_much_i_eat = 2*my_weight/700;
+        what_i_eat = 'c';
+        is_tame = false;
+        break;
+      }
+    case 'N' :
+      {
+        size=2;
+        hab=new char[size];
+        hab[0]='x';
+        hab[1]='#';
 
-                interaction = "Grooowwll";
-                srand(time(NULL));
-                my_weight = 1400 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(3200-1400)));
-                how_much_i_eat = 40*my_weight/2400;
-                what_i_eat = 'h';
-                is_tame = true;
-                break;
-            }
-    }
+        interaction = "Grooowwll";
+        srand(time(NULL));
+        my_weight = 1400 + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(3200-1400)));
+        how_much_i_eat = 40*my_weight/2400;
+        what_i_eat = 'h';
+        is_tame = true;
+        break;
+      }
+  }
 }
 
 Animal& Animal::operator=(const Animal& a){
-    x= a.x;
-    y = a.y;
-    content=a.content;
+  x= a.x;
+  y = a.y;
+  content=a.content;
     
-    size=a.size;
-    hab=new char[size];
-    for (int i=0;i<size;i++)
-    {
-        hab[i]=a.hab[i];
-    }
+  size=a.size;
+  hab=new char[size];
+  for (int i=0;i<size;i++)
+  {
+    hab[i]=a.hab[i];
+  }
 
-    cage_id = a.cage_id;
-    interaction = a.interaction;
-    my_weight = a.my_weight;
-    how_much_i_eat = a.how_much_i_eat;
-    what_i_eat = a.what_i_eat;
-    is_tame = a.is_tame;
-    return *this;
+  cage_id = a.cage_id;
+  interaction = a.interaction;
+  my_weight = a.my_weight;
+  how_much_i_eat = a.how_much_i_eat;
+  what_i_eat = a.what_i_eat;
+  is_tame = a.is_tame;
+  return *this;
 }
 
 void Animal::move(int dir){
-    switch(dir){
+  switch(dir)
+  {
     case 1: //top
-        y -= 1;
-        break;
-    case 2: //right
-        x += 1;
-        break;
-    case 3: //down
-        y += 1;
-        break;
-    case 4: //left
-        x -= 1;
-        break;
+    {
+      y -= 1;
+      break;
     }
+    case 2: //right
+    {
+      x += 1;
+      break;
+    }
+    case 3: //down
+    {
+      y += 1;
+      break;
+    }
+    case 4: //left
+    {
+      x -= 1;
+      break;
+    }
+  }
 }
 
 int Animal::GetSize()
 {
-    return size;
-}
-
+  return size;
+}  
 char Animal::GetContent()
 {
-    return content;
+  return content;
 }
 
 string Animal::interact()
 {
-    return this->interaction;
+  return this->interaction;
 }
 
 float Animal::GetWeight()
 {
-    return my_weight;
+  return my_weight;
 }
 
 float Animal::GetEat()
 {
-    return how_much_i_eat;
+  return how_much_i_eat;
 }
 
 int Animal::GetID()
 {
-    return cage_id;
+  return cage_id;
 }
 
 int Animal::GetWEat()
 {
-    return what_i_eat;
+  return what_i_eat;
 }
 
 int Animal::GetX()
 {
-    return x;
+  return x;
 }
 
 int Animal::GetY()
 {
-    return y;
+  return y;
 }
 
 bool Animal::GetTame()
 {
-    return is_tame;
+  return is_tame;
 }
 
 char* Animal::GetHab()
 {
-    return hab;
+  return hab;
 }
--- v2/src/cage.cpp
+++ v2/src/cage.cpp
@@ -6,107 +6,107 @@ int neff = -1;
 
 Cage::Cage(int _size, vector<Habitat> buf) : size(buf.size())
 {
-	h = new Habitat[buf.size()];
-	for (int i = 0; i < size; i++)
-	{
-		h[i] = buf[i];
-	}
+  h = new Habitat[buf.size()];
+  for (int i = 0; i < size; i++)
+  {
+    h[i] = buf[i];
+  }
 	
-	float temp_many_animal = 0.3 * size;
-	int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
-	a = new Animal*[many_animal];
+  float temp_many_animal = 0.3 * size;
+  int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
+  a = new Animal*[many_animal];
 }
 
 Cage::Cage(const Cage& c) : size(c.size)
 {
-	h = new Habitat[c.size];
-	int i;
-	for (i = 0; i < size; i++)
-	{
-		h[i] = c.h[i];
-	}
+  h = new Habitat[c.size];
+  int i;
+  for (i = 0; i < size; i++)
+  {
+    h[i] = c.h[i];
+  }
 
-	float temp_many_animal = 0.3 * c.size;
-	int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
-	a = new Animal*[many_animal];
+  float temp_many_animal = 0.3 * c.size;
+  int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
+  a = new Animal*[many_animal];
 
-	for (i = 0; i < many_animal; i++)
-	{
-		a[i] = c.a[i];
-	}
+  for (i = 0; i < many_animal; i++)
+  {
+    a[i] = c.a[i];
+  }
 }
 
 Cage::~Cage()
 {
-	delete [] h;
-	delete [] a;
+  delete [] h;
+  delete [] a;
 }
 
 Cage& Cage::operator=(const Cage& c)
 {
-	delete [] h;
-	delete [] a;
-	h = new Habitat[c.size];
-	int i;
-	for (i = 0; i < size; i++)
-	{
-		h[i] = c.h[i];
-	}
+  delete [] h;
+  delete [] a;
+  h = new Habitat[c.size];
+  int i;
+  for (i = 0; i < size; i++)
+  {
+    h[i] = c.h[i];
+  }
 
-	float temp_many_animal = 0.3 * c.size;
-	int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
-	a = new Animal*[many_animal];
+  float temp_many_animal = 0.3 * c.size;
+  int many_animal = floor(temp_many_animal); // perlu casting ke int ga?
+  a = new Animal*[many_animal];
 
-	for (i = 0; i < many_animal; i++)
-	{
-		a[i] = c.a[i];
-	}
+  for (i = 0; i < many_animal; i++)
+  {
+    a[i] = c.a[i];
+  }
 
-	return *this;
+  return *this;
 }
 
 void Cage::AddAnimal(char animal, int i)
 {	
-	int posid;
-	srand(time(NULL));
-	do
-	{
-		posid = rand() % size;
-	} while (!ContainAnimal(h[posid].GetCellRow(), h[posid].GetCellCol()));
+  int posid;
+  srand(time(NULL));
+  do
+  {
+    posid = rand() % size;
+  } while (!ContainAnimal(h[posid].GetCellRow(), h[posid].GetCellCol()));
 
-	a[neff++] = new Animal(h[posid].GetCellRow(), h[posid].GetCellCol(), i,animal);
+  a[neff++] = new Animal(h[posid].GetCellRow(), h[posid].GetCellCol(), i,animal);
 }
 
 bool Cage::ContainAnimal(int x, int y)
 {
-	float temp_many_animal = 0.3 * size;
-	int many_animal = floor(temp_many_animal);
-	bool found = false;
-	int i = 0;
+  float temp_many_animal = 0.3 * size;
+  int many_animal = floor(temp_many_animal);
+  bool found = false;
+  int i = 0;
 
-	while ((i < many_animal) && (!found))
-	{
-		if ((x == a[i]->GetX()) && (y == a[i]->GetY()))
-		{
-			found = true;
-		}
-		else
-		{
-			i++;
-		}
-	}
+  while ((i < many_animal) && (!found))
+  {
+    if ((x == a[i]->GetX()) && (y == a[i]->GetY()))
+    {
+      found = true;
+    }
+    else
+    {
+      i++;
+    }
+  }
 
-	return found;
+  return found;
 }
 
 bool Cage::IsFull()
 {
-	float temp_many_animal = 0.3 * size;
-	int many_animal = floor(temp_many_animal);
-	return (many_animal == neff);
+  float temp_many_animal = 0.3 * size;
+  int many_animal = floor(temp_many_animal);
+  return (many_animal == neff);
 }
 
 bool Cage::IsEmpty()
 {
-	return (neff == -1);
+  return (neff == -1);
 }
