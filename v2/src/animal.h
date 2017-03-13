fde9ba8377709e8f08e87f61ef8ef6fb4899a2f3
adrianhp97 <adrianhp100797@gmail.com>
2017-03-13 22:41:25 +0700

    header

--- v2/src/animal.h
+++ v2/src/animal.h
@@ -7,85 +7,70 @@ using namespace std;
 
 class Animal {
 public:
-	/**
-	 * @brief Constructor.
-	 * Menciptakan Animal kosong.
-	 */
-	Animal(int _x, int _y, int id);
+  /**
+   * @brief Constructor.
+   * Menciptakan Animal kosong.
+   */
+  Animal(int _x, int _y, int id);
+  /**
+   * @brief Overloading Operator "="
+   * Menambahkan fungsi pada operator "=" agar penyalinan tidak terjadi secara bit wise
+   * @param a adalah Animal yang hendak disalin
+   */
+  Animal& operator=(const Animal& a);
 
-	/**
-	 * @brief CopyConstructor.
-	 * Membuat salinan dari Animal lain ke objek
-	 * @param c adalah Animal yang hendak disalin
-	 */
-//	Animal(const Animal& a);
-//  Sepertinya tidak perlu, tinggal di bitwise copy
+  /**
+  * @brief Function interact
+  * Interact pada binatang berupa string
+  */
+  string interact();
+  /**
+   * @brief Procedure move
+   * Pergerakan binatang dari point ke point yang dituju berdasarkan int yang dipilih
+   * @param int dir adalah pilihan swicth untuk 1.moveup 2.moveright 3.movedown 4.moveleft
+   */
+  void move(int);
 
-	/**
-	 * @brief Destructor.
-	 */
-//	~Animal();
-//  Tidak ada variabel dinamik
+  int GetSize();
 
-	/**
-	 * @brief Overloading Operator "="
-	 * Menambahkan fungsi pada operator "=" agar penyalinan tidak terjadi secara bit wise
-	 * @param a adalah Animal yang hendak disalin
-	 */
-	Animal& operator=(const Animal& a);
+  char GetContent();
 
-	/**
-	* @brief Function interact
-	* Interact pada binatang berupa string
-	*/
-	string interact();
-	/**
-	 * @brief Procedure move
-	 * Pergerakan binatang dari point ke point yang dituju berdasarkan int yang dipilih
-	 * @param int dir adalah pilihan swicth untuk 1.moveup 2.moveright 3.movedown 4.moveleft
-	 */
-	void move(int);
+  string interact();
 
-	int GetSize();
+  float GetWeight();
 
-	char GetContent();
+  float GetEat();
 
-	string interact();
+  int GetID();
 
-	float GetWeight();
+  int GetWEat();
 
-	float GetEat();
-
-	int GetID();
-
-	int GetWEat();
-
-	/**
-	* @brief Function GetX()
-	* Getter x
-	*/
-	int GetX();
+  /**
+  * @brief Function GetX()
+  * Getter x
+  */
+  int GetX();
 	
-	/**
-	* @brief Function GetY()
-	* Getter y
-	*/
-	int GetY();
+  /**
+  * @brief Function GetY()
+  * Getter y
+  */
+  int GetY();
 
-	bool GetTame();
+  bool GetTame();
 
-	char* GetHab();
+  char* GetHab();
 
 protected:
-	string interaction;
-	char content;
-    int x;
-    int y;
-    int cageID;
-	float myWeight;
-	float howMuchIEat; // ini dihitung dari myWeight
-	char whatIEat;
-	bool isTame;
-	char* hab;
+  string interaction;
+  char content;
+  int x;
+  int y;
+  int cage_id;
+  float my_weight;
+  float how_much_i_eat; // ini dihitung dari myWeight
+  char what_i_eat;
+  bool is_tame;
+  char* hab;
 };
 #endif
